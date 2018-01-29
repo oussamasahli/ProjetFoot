a9b889b1cc054899d78b55a2cf6171da254070d6
baskiotisn <nicolas.baskiotis@lip6.fr>
2018-01-29 18:07:25 +0100

    challenge __init__

--- Exemple_GIT_REPO/__init__.py
+++ Exemple_GIT_REPO/__init__.py
@@ -5,4 +5,10 @@ def get_team(nb_players):
     myteam = SoccerTeam(name="MaTeam")
     for i in range(nb_players):
         myteam.add("Joueur "+str(i) ,RandomStrategy())
-    return myteam
+    return myteam	
+
+def get_team_challenge(num):
+	myteam = SoccerTeam(name="MaTeamChallenge")
+	if num == 1:
+		myteam.add("Joueur Chal "+str(num),RandomStrategy())
+	return myteam
