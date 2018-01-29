a01333ef1a3c80677884950bc4a7b11fcf1ee203
baskiotisn <nicolas.baskiotis@lip6.fr>
2018-01-29 18:18:05 +0100

    challenge exemple

--- Exemple_GIT_REPO/__init__.py
+++ Exemple_GIT_REPO/__init__.py
@@ -3,8 +3,16 @@ from soccersimulator import SoccerTeam
 
 def get_team(nb_players):
     myteam = SoccerTeam(name="MaTeam")
-    for i in range(nb_players):
-        myteam.add("Joueur "+str(i) ,RandomStrategy())
+    if nb_players == 1:
+        myteam.add("Joueur " ,RandomStrategy())
+    if nb_players == 2:
+	myteam.add("Joueur 1", RandomStrategy())
+	myteam.add("Joueur 2", RandomStrategy())
+    if nb_players == 4:
+	myteam.add("Joueur 1",RandomStrategy())
+	myteam.add("Joueur 2",RandomStrategy())
+	myteam.add("Joueur 3",RandomStrategy())
+	myteam.add("Joueur 4",RandomStrategy())
     return myteam	
 
 def get_team_challenge(num):
