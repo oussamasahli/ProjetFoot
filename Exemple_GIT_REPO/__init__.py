806d84d373f331d3d01cf42e8d7f008fb1c6402f
baskiotisn <nicolas.baskiotis@gmail.com>
2018-02-02 01:21:07 +0100

    debug__init__

--- Exemple_GIT_REPO/__init__.py
+++ Exemple_GIT_REPO/__init__.py
@@ -6,13 +6,13 @@ def get_team(nb_players):
     if nb_players == 1:
         myteam.add("Joueur " ,RandomStrategy())
     if nb_players == 2:
-	myteam.add("Joueur 1", RandomStrategy())
-	myteam.add("Joueur 2", RandomStrategy())
+		myteam.add("Joueur 1", RandomStrategy())
+		myteam.add("Joueur 2", RandomStrategy())
     if nb_players == 4:
-	myteam.add("Joueur 1",RandomStrategy())
-	myteam.add("Joueur 2",RandomStrategy())
-	myteam.add("Joueur 3",RandomStrategy())
-	myteam.add("Joueur 4",RandomStrategy())
+		myteam.add("Joueur 1",RandomStrategy())
+		myteam.add("Joueur 2",RandomStrategy())
+		myteam.add("Joueur 3",RandomStrategy())
+		myteam.add("Joueur 4",RandomStrategy())
     return myteam	
 
 def get_team_challenge(num):
